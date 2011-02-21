00d368a7e3dc125406bc1cf088a58ae259ee6283
FingerDemon <RichWard@cox.net>
2011-02-21 14:29:06 -0500

    Initial commit

---
+++ rails_projects/quick_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickApp::Application.config.secret_token = '37e9a40612320c048ee0f1809995c6af5b66e1516bf52a48bf41f6c0b7f687a6d3302791b517ba9170852434c12f8a43ce0c093ed9742b5508ccb4fa8b8b6d60'
